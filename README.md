37ad6b05d60ebc9a734e49e0dd078da7ec0e7d7a
DecSP <nguyencaoson7921@gmail.com>
2022-02-14 14:21:49 +0700

    No need phpsessid manually

--- README.md
+++ README.md
@@ -9,13 +9,6 @@ The site provides my solutions for coding problems of CSES site that is owned by
 <img src="public/og.png" width="600px">
 
 ## How to download your CSES solutions and/or package them into web interface:
-You actually only need crawler.py (and htmlgen.py, style.css, content.css if you want to package solutions) for this. You might want to create an empty directory and put only those files inside. Please follow this steps:
-- Get your SESSID:
-    - Go to CSES login page 
-    - Press F12 to open Browser Dev Tools and go to Network tab
-    - Fill the information and login
-    - Look at the Browser Dev Tools and find the details of your login request and go to cookies, you will find the field "PHPSESSID"
-    - Copy the value and paste it into crawler.py where "YOUR_SESSID_HERE" is specified.
-- Download your solutions using crawler.py:
+You actually only need crawler.py (and htmlgen.py, style.css, content.css if you want to package solutions) for this. You might want to create an empty directory and put only those files inside. Then:
     - `python crawler.py` to download all problems
     - `python crawler.py <problemID>` to download a specific problem
--- crawler.py
+++ crawler.py
@@ -7,16 +7,35 @@ from collections import defaultdict
 import sys
 
 url='https://cses.fi/problemset'
-cookies={'PHPSESSID':'YOUR_SESSID_HERE'}
+cookies={}
 
-def download_url(url,needcookies=False):
-    if needcookies:
-        return requests.get(url,cookies=cookies).text
-    else: return requests.get(url).text
+def download_url(s,url):
+    return requests.get(url,cookies=cookies).text
+
+def login(s):
+    print('Username: ',end='')
+    username=input()
+    print('Password: ',end='')
+    password=input()
+    url='https://cses.fi/login'
+    payload={'nick':username,
+            'pass':password,
+            }
+    p=s.get(url)
+    cookies['PHPSESSID']=p.cookies['PHPSESSID']
+    html=p.text
+    soup=bs(html,'html.parser')
+    payload['csrf_token']=soup.find('input',{'name':'csrf_token'})['value']
+    p=s.post(url,data=payload)
+    soup=bs(p.text,'html.parser')
+    if soup.find('a',{"class":"account"})["href"]=="/login":
+        print('Log in failed!')
+        exit(0)
+    print('Logged in successfully')
 
 class Problem:
-    def __init__(self,probUrl):
-        html=download_url(probUrl,True)    
+    def __init__(self,s,probUrl):
+        html=download_url(s,probUrl) 
         self.url=probUrl
         self.soup = bs(html, 'html.parser')
         self.aclink = None
@@ -26,7 +45,7 @@ class Problem:
         self.accodelang = None
         self.title = self.soup.h1.getText()
         self.cate = self.soup.h4.getText()
-
+        print('- Fetching: %s > %s'%(self.cate,self.title))
         for link in self.soup.find_all('a'):
             path = link.get('href')
             if path and path.startswith('/problemset/result'):
@@ -34,13 +53,13 @@ class Problem:
                 if not link.span: continue
                 if 'full' in link.span.get('class'):
                     self.aclink=path
-                    self.getSubmission(path)
-                    print('- Got: %s > %s'%(self.cate,self.title))
-                    print('  Language: %s / Submission time: %s'%(self.accodelang,self.actime))
-                    break
+                    self.getSubmission(s,path)
+                    print(' -> Status: Solved / %s / %s'%(self.accodelang,self.actime))
+                    return
+        print(' -> Status: Not Solved')
         
-    def getSubmission(self,resultUrl):
-        html=download_url(resultUrl,True)
+    def getSubmission(self,s,resultUrl):
+        html=download_url(s,resultUrl)
         self.soupac=bs(html,'html.parser')
         self.actime=self.soupac.table.find_all('tr')[2].find_all('td')[1].getText()
         lang=self.soupac.table.find_all('tr')[3].find_all('td')[1].getText()
@@ -55,9 +74,9 @@ class Problem:
         self.accodehtml['class']="language-%s"%self.accodelang
         self.accode=tmp.getText()
 
-def getProblems(urls):
+def getProblems(s,urls):
     for url in urls:
-        yield(Problem(url))
+        yield(Problem(s,url))
 
 def writeToFile(filename,content):
     os.makedirs(os.path.dirname(filename), exist_ok=True)
@@ -75,72 +94,76 @@ def writeCodeToDirectory(problem):
     writeToFile(filename,problem.accode)
 
 def main(argv):
-    html=download_url(url)
-    soup=bs(html,'html.parser')
-    problemUrls=[]
-    for link in soup.find_all('a'):
-        path = link.get('href')
-        if path and path.startswith('/problemset/task'):
-            problemUrls.append(urljoin(url, path))
+    with requests.Session() as s:
+        html=download_url(s,url)
+        soup=bs(html,'html.parser')
+        problemUrls=[]
+        for link in soup.find_all('a'):
+            path = link.get('href')
+            if path and path.startswith('/problemset/task'):
+                problemUrls.append(urljoin(url, path))
 
-    def asking():
-        # ask if want to download solution
-        solFlag=False
-        print("Do you want to download solution code (y/N)? ",end="")
-        if input().strip().lower()=='y': solFlag=True
+        def asking():
+            # ask if want to download solution
+            solFlag=False
+            print("Do you want to download solution code (y/N)? ",end="")
+            if input().strip().lower()=='y': solFlag=True
 
-        # ask if want to package the solutions to an web interface (open index.html to view)
-        packageFlag=False
-        print("Do you want to package solution code to web interface (y/N)? ",end="")
-        if input().strip().lower()=='y': packageFlag=True
-        print("-------------------------------------------------------------")
-        return (solFlag,packageFlag)
+            # ask if want to package the solutions to an web interface (open index.html to view)
+            packageFlag=False
+            print("Do you want to package solution code to web interface (y/N)? ",end="")
+            if input().strip().lower()=='y': packageFlag=True
+            print("-------------------------------------------------------------")
+            return (solFlag,packageFlag)
 
 
-    # no args
-    if len(argv)==1:
-        solFlag,packageFlag=asking()
-        sidebar=defaultdict(list)
-        for problem in getProblems(problemUrls):
-            if not problem.accodelang: continue
-            sidebar[problem.cate].append(problem.title)
-            
-            filename='%s/%s.html'%(problem.cate,problem.title)
+        # no args
+        if len(argv)==1:
+            login(s)
+            solFlag,packageFlag=asking()
+            sidebar=defaultdict(list)
+            cnt=0
+            for problem in getProblems(s,problemUrls):
+                if not problem.accodelang: 
+                    continue
+                sidebar[problem.cate].append(problem.title)
+                
+                filename='%s/%s.html'%(problem.cate,problem.title)
+                if solFlag:
+                    writeCodeToDirectory(problem)
+                if packageFlag:
+                    writeToFile(filename,genHtmlProblem((problem.title,problem.url,str(problem.accodehtml))))
+                cnt+=1
+            if solFlag:
+                print('-> Save code successfully')
+            if packageFlag:
+                writeToFile("./index.html",genHtmlIndex(sidebar))
+                print('-> Generated index.html successfully')
+            print("You solved %d problems"%cnt)
+
+        # args = [problemID]
+        elif len(argv)==2:
+            login(s)
+            probUrl='https://cses.fi/problemset/task/%s'%argv[1]
+            if not probUrl in problemUrls:
+                print("ProblemID not found")
+                return
+            solFlag,packageFlag=asking()
+            problem=Problem(s,probUrl)
             if solFlag:
                 writeCodeToDirectory(problem)
-                print('->Save code successfully')
+                print('-> Save code successfully')
             if packageFlag:
                 writeToFile(filename,genHtmlProblem((problem.title,problem.url,str(problem.accodehtml))))
-                print('->Save html code successfully')
-
-        if packageFlag:
-            writeToFile("./index.html",genHtmlIndex(sidebar))
-            print('->Generated index.html successfully')
-
-    # args = [problemID]
-    elif len(argv)==2:
-        probUrl='https://cses.fi/problemset/task/%s'%argv[1]
-        if problemUrls.index(probUrl)==-1:
-            print("ProblemID not found")
-            return
-        solFlag,packageFlag=asking()
-        problem=Problem(probUrl)
-        if solFlag:
-            writeCodeToDirectory(problem)
-            print('->Save code successfully')
-        if packageFlag:
-            writeToFile(filename,genHtmlProblem((problem.title,problem.url,str(problem.accodehtml))))
-            print('->Save html code successfully')
-    else: printUsage()
+                print('-> Save html code successfully')
+        else: printUsage()
 
 def printUsage():
     print("""Usage:
     `python crawler.py` to download all problems
     `python crawler.py <problemID>` to download a specific problem
-    Notice: Remember to get your SESSID and put it in the code
 """)
 
 
 if __name__ == "__main__":
-    if cookies['PHPSESSID']=='YOUR_SESSID_HERE': printUsage()
-    else: main(sys.argv)
+    main(sys.argv)
